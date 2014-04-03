39fb36271cecad65957d0bac358a03ef6958db30
HS Yang <YG@FourwingsY.local>
2014-04-04 02:59:43 +0900

    edit crawler

--- Crawler/crawler.py
+++ Crawler/crawler.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # FILE_NAME: crawler.py
-'''
-import joongang_article_parser as joongang
-import hankyung_article_parser as hankyung
-'''
+
+# ARTICLE PARSERs
 import donga_article_parser as donga
 import hani_article_parser as hani
 import joongang_article_parser as joongang
 import hankyung_article_parser as hankyung
-
-import sys
 # and so on
 
+# MySQL Connector
 import DB_connector as db
+
+# Python Libraries
+import sys
 from contextlib import closing
 
-#press_list = [joongang, hankyung, donga]
 press_list = [donga, hani, joongang, hankyung]
 
 # NEEDS MANUAL HANDLING
@@ -24,26 +23,31 @@ def main(argv) :
     press_index = int(argv[1])-1
     start_page_index = int(argv[2])
     end_page_index = int(argv[3])
-
     print press_index
+    
     press = press_list[press_index]
-    press_id = press.get_press_id()
+    press_name = press.get_press_name()
+    print press_name
+    
     con = db.connect()
-
     with closing(con.cursor()) as cur :
         for i in range(start_page_index, end_page_index+1) :
             # get 10-20 url and insert
             url_list = press.get_article_urls_with_pagenum(i)
+            print "page: " + i
 
             for url in url_list :
                 article = press.parse_article_with_url(url)
-                author_info = article['author_info']
                 query = db.make_insert_query("article", article)
-                #print query
                 cur.execute(query)
                 con.commit()
 
+                author_info = article['author_info']
+                do_something_with_author_info(author_info)
     con.close()
-    
+
+def do_something_with_author_info(author_info) :
+    return None
+
 if __name__ == "__main__":
     main(sys.argv)
--- Crawler/donga_article_parser.py
+++ Crawler/donga_article_parser.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import mysql.connector
-from mysql.connector import errorcode
-import sys
+
 import urllib
-import json
 import re
 from bs4 import BeautifulSoup
 
-def get_press_id() :
+def get_press_name() :
 	return "donga"
 	
 def get_article_urls_with_pagenum(page) :
-	print "PAGE NUMBER : " + str(page)
-	domain = "http://news.donga.com/List?p="
-	pressName = "동아"
+	# print "PAGE NUMBER : " + str(page)
+	LIST_URL = "http://news.donga.com/List?p="
 	number_of_article_per_page = 16
 	article_list_URL = domain + str( 1 + number_of_article_per_page * ( page - 1 ) )	
 	# 동아일보 기사만
-	#article_list_URL += "&ymd=&m=NP"
+	article_list_URL += "&m=NP"
 
 	# 파싱할 개별 기사의 URL들을 확보하기
-	article_URLs = getArticleURLs(article_list_URL)	
+	article_URLs = get_article_URLs(article_list_URL)	
 
 	'''
 	article_urls = []
@@ -35,10 +31,11 @@ def get_article_urls_with_pagenum(page) :
 	'''
 	return article_URLs
 
-def getArticleURLs(article_list_URL) :
+def get_article_URLs(article_list_URL) :
 	url_opener = urllib.urlopen(article_list_URL)
 	url_opener.addheaders = [("User-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.74.9 (KHTML, like Gecko) Version/7.0.2 Safari/537.74.9")]
-	html_doc = url_opener.read()	
+	html_doc = url_opener.read()
+	url_opener.close()
 
 	soup = BeautifulSoup(html_doc)
 	news_list = soup.find_all("div", "articleList")
@@ -49,7 +46,58 @@ def getArticleURLs(article_list_URL) :
 	
 	return URLs
 
-def extract_section(html_doc) :
+def parse_article_with_url(URL): 
+	html_doc = urllib.urlopen(URL).read()	
+	article = {}
+#	article['press'] = "donga"
+	article['URL'] = URL.encode('utf-8')
+	article["title"] = _extract_title(html_doc)
+	if article["title"] is None:
+		return None
+	article["datetime"] = _extract_datetime(html_doc)
+	#article["modified"] = article_soup.find("p","title_foot").find("span","date2").contents[0].strip()
+	# SECTION 검출
+	article["section"] = _extract_section(html_doc)						
+	article["content"] = _extract_contents(html_doc)
+	article["author_info"] = _extract_author_info(html_doc)
+	article["press_id"] = 1
+	article["is_email_exist"] = 1
+
+	# 기자이름을 발견하지 못할 시 관련 정보 콘솔에 출력
+	if(article["author_info"] == "") :
+		_no_author_info_error(article)   
+	print "기사 읽는 중 ... " + article["author_info"]
+
+	'''
+	print u"--제목--- "+ article["title"]		
+	print u"--주소--- "+ article["URL"]
+	print u"--기자--- "+ article["author_info"]
+	'''
+	return article
+
+def _no_author_info_error(article) :
+	dead = article["title"].find("[부고]")
+	job = article["title"].find("[인사]")
+	channel_a = article["title"].find("[오늘의 채널A]")
+	hot_donga = article["title"].find("[화제의 동아닷컴]")
+	channel_a_news = article["content"].find("채널A 뉴스")
+	channel_a_news2 = article["content"].find("채널A뉴스")
+	board = article["content"].find("게시판")
+	woman = article["content"].find("[우먼 동아일보]")	  
+	if( dead == -1 & job == -1 & channel_a == -1 & hot_donga == -1 & channel_a_news == -1 & channel_a_news2 == -1 & board == -1 & woman == -1):
+		text_file = open("Author_not_found.txt", "a")
+		text_file.write("\n\n------!!!!!------- 기자를 찾지못한 기사입니다 ------!!!!!------")
+		text_file.write("\n--제목--" + article["title"])
+		text_file.write("\n--주소--" + article["URL"])
+		text_file.write("\n--내용--" + article["content"])
+		text_file.close()
+		print "------!!!!!------- 기자를 찾지못한 기사입니다 ------!!!!!------"
+		print "--내용--- "+ article["content"]	
+		print "--제목--- "+ article["title"]		
+		print "--주소--- "+ article["URL"]
+		print "------!!!!!------- ----- ----- ----- ----- --- ------!!!!!------" 
+
+def _extract_section(html_doc) :
 	article_soup = BeautifulSoup(html_doc)
 	article_section = article_soup.find("p","location")
 	if( len(article_section.strong.contents) > 0) :
@@ -61,19 +109,19 @@ def extract_section(html_doc) :
 	section = section[5:]
 	return section.encode('utf-8')
 
-def extract_title(html_doc) :
+def _extract_title(html_doc) :
 	article_soup = BeautifulSoup(html_doc)
 	if article_soup.find("div","article_title").h1 is None:
 		return None
 	title = article_soup.find("div","article_title").h1.contents[0].strip()
 	return title.encode('utf-8')
 
-def extract_datetime(html_doc) :
+def _extract_datetime(html_doc) :
 	article_soup = BeautifulSoup(html_doc)
 	datetime = article_soup.find("p","title_foot").find("span","date").contents[0].strip()
 	return datetime
 
-def extract_contents(html_doc) :
+def _extract_contents(html_doc) :
 	article_soup = BeautifulSoup(html_doc)
 	# 본문 내용에 해당하는 텍스트들의 리스트 확보. 관련기사 항목. 사진박스는 삭제
 	article_relation = article_soup.find("div","article_relation")
@@ -83,10 +131,10 @@ def extract_contents(html_doc) :
 	if(art_Rel is not None):
 		art_Rel.clear()
 		
-	articlePhotos = article_soup.findAll("div","articlePhotoC")
-	if(articlePhotos is not None):
-		for articlePhoto in articlePhotos:
-			articlePhoto.clear()		
+	article_photos = article_soup.findAll("div","articlePhotoC")
+	if(article_photos is not None):
+		for article_photo in article_photos:
+			article_photo.clear()		
 	# 컨텐츠 텍스트 리스트를 모두 합친 내용 문자열을 만듬. idx_of_author_line 이후의 내용들은 대상에서 제외		
 	content_text_element_list = article_soup.find("div","article_txt").findAll(text=True)
 	
@@ -98,7 +146,7 @@ def extract_contents(html_doc) :
 	content = text_total.strip()	
 	return content.encode('utf-8')
 
-def extract_author(html_doc) :
+def _extract_author_info(html_doc) :
 	article_soup = BeautifulSoup(html_doc)
 	# 본문 내용에 해당하는 텍스트들의 리스트 확보. 관련기사 항목. 사진박스는 삭제
 	article_relation = article_soup.find("div","article_relation")
@@ -108,10 +156,10 @@ def extract_author(html_doc) :
 	if(art_Rel is not None):
 		art_Rel.clear()
 		
-	articlePhotos = article_soup.findAll("div","articlePhotoC")
-	if(articlePhotos is not None):
-		for articlePhoto in articlePhotos:
-			articlePhoto.clear()		
+	article_photos = article_soup.findAll("div","articlePhotoC")
+	if(article_photos is not None):
+		for article_photo in article_photos:
+			article_photo.clear()		
 		
 	content_text_element_list = article_soup.find("div","article_txt").findAll(text=True)
 	
@@ -125,7 +173,7 @@ def extract_author(html_doc) :
 	#article["author_keyword"] = None
 	content_text_element_list_r = content_text_element_list[::-1]
 	for idx, text_element_item in enumerate(content_text_element_list_r):
-		author_keyword = isNameOccured(text_element_item)
+		author_keyword = __is_name_occured(text_element_item)
 		if(author_keyword is not None) :
 			idx_of_author_line = idx
 			writter_found = 1
@@ -134,55 +182,7 @@ def extract_author(html_doc) :
 			break    
 	return author.encode('utf-8')
 
-def parse_article_with_url(URL): 
-	html_doc = urllib.urlopen(URL).read()	
-	article = {}
-#	article['press'] = "donga"
-	article['URL'] = URL.encode('utf-8')
-	article["title"] = extract_title(html_doc)
-	if article["title"] is None:
-		return None
-	article["datetime"] = extract_datetime(html_doc)
-	#article["modified"] = article_soup.find("p","title_foot").find("span","date2").contents[0].strip()
-	# SECTION 검출
-	article["section"] = extract_section(html_doc)						
-	article["content"] = extract_contents(html_doc)
-	article["author_info"] = extract_author(html_doc)
-	article["press_id"] = 1
-	article["is_email_exist"] = 1
-
-	# 기자이름을 발견하지 못할 시 관련 정보 콘솔에 출력
-	if(article["author_info"] == "") :
-		dead = article["title"].find("[부고]")
-		job = article["title"].find("[인사]")
-		channel_a = article["title"].find("[오늘의 채널A]")
-		hot_donga = article["title"].find("[화제의 동아닷컴]")
-		channel_a_news = article["content"].find("채널A 뉴스")
-		channel_a_news2 = article["content"].find("채널A뉴스")
-		board = article["content"].find("게시판")
-		woman = article["content"].find("[우먼 동아일보]")	  
-		if( dead == -1 & job == -1 & channel_a == -1 & hot_donga == -1 & channel_a_news == -1 & channel_a_news2 == -1 & board == -1 & woman == -1):
-			text_file = open("Author_not_found.txt", "a")
-			text_file.write("\n\n------!!!!!------- 기자를 찾지못한 기사입니다 ------!!!!!------")
-			text_file.write("\n--제목--" + article["title"])
-			text_file.write("\n--주소--" + article["URL"])
-			text_file.write("\n--내용--" + article["content"])
-			text_file.close()
-			print "------!!!!!------- 기자를 찾지못한 기사입니다 ------!!!!!------"
-			print "--내용--- "+ article["content"]	
-			print "--제목--- "+ article["title"]		
-			print "--주소--- "+ article["URL"]
-			print "------!!!!!------- ----- ----- ----- ----- --- ------!!!!!------"    
-	print "기사 읽는 중 ... " + article["author_info"]
-
-	'''
-	print u"--제목--- "+ article["title"]		
-	print u"--주소--- "+ article["URL"]
-	print u"--기자--- "+ article["author_info"]
-	'''
-	return article
-
-def isNameOccured(line_text) :
+def __is_name_occured(line_text) :
 
 	keyword_array = [u'디지털뉴스팀',u'영상뉴스팀',u'도깨비뉴스',u'동아오토', u'[스포츠동아]', u'.[스포츠동아]', u' 논설위원 ', u' 특파원 ', u' 인턴기자 ', u' 전문기자 ', u' 동아닷컴 기자 ',u' 통신원 ',u'스포츠동아 해설위원', u' 객원논설위원', u'=뉴스1)', u' 뉴스1', u'=뉴시스】', u'뉴시스',u'=신화/뉴시스】', u'신화 뉴시스', u'. 뉴시스',u'우한=AP', u' 뉴시스',u'로스앤젤레스=',u'진주=', u'[뉴스엔]', u'StartFragment', u'남양주=', u'기앙쿠르=', u'천안=',u'도쿄=',u'뉴욕=',u'부산=', u'파리=', u'창원｜', u'잠실｜',u'세종=',u'부리람(태국)｜',u'상주｜', u'마산｜', u'대구｜', u'창원｜',u'하이커우(중국 하이난성)｜', u' 뉴욕=', u'예산=', u'김천=', u'천안=', u'하이커우=' ,u'서귀포=', u'경주=', u'청주=', u'아산=',u'베이징=',u'구례=', u'옥천·영동=',u'음성=',u'광주=', u'안성=',u'포항=', u'고창=',u'.베이징=' ,u'워싱턴=', u'.워싱턴=', u'=AP', u'제네바=' u'창원=',u' 창원=', u'하이코우=', u'/ IT동',u'·정책사회부', u'[엔터테인먼트부]', u' 에세이스트',u'소치=', u'경기 화성=', u'특약=', u'<본 자료는 해당기관에서 제공한 보도 자료입니다.>', u'원성열', u'담당·', u'@donga.co', u' 번역가', u'뉴스1', u'홈플러스 제공', u'박상준 기자', u'전영희 기자', u'제네바=정진수 동아닷컴 기자',u'주영로 기자',u'부산｜', u'sereno@donga.com', u'<동아닷컴>', u'동아바둑', u'윤양섭 전문기자', u'사진·문형일 기자', u' 기자 ', u' 동아닷컴 ',u'로이터·동아닷컴 특약']
 	for idx, keyword in enumerate(keyword_array):
@@ -192,7 +192,10 @@ def isNameOccured(line_text) :
 			return keyword
 	return None
 
-def insertArticleToDB(articles):	
+def insertArticleToDB(articles):
+	import mysql.connector
+	from mysql.connector import errorcode
+
 	#articles = []
 
 	config = {
@@ -271,6 +274,7 @@ def main(pagenum) :
 			articles.append(article)
 	return articles
 '''
+import sys
 start_page_index = int(sys.argv[1])
 end_page_index = int(sys.argv[2])
 
