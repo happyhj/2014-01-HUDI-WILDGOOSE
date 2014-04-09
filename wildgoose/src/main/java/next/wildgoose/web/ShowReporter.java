180b0f72f1187aea9802aaf045d9adffd09fa528
FourwingsY <fourwingsy@gmail.com>
2014-04-09 11:45:48 +0900

    inf-loop Error fixed

--- wildgoose/src/main/java/next/wildgoose/web/ShowReporter.java
+++ wildgoose/src/main/java/next/wildgoose/web/ShowReporter.java
@@ -1,6 +1,8 @@
 package next.wildgoose.web;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,8 +26,15 @@ public class ShowReporter extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		/* 
+		 * getRequestURI() : /reporters/HBKim1
+		 * getRequestURL() : http://10.73.45.145:8080/reporters/HBKim1
+		 */
+		String uri = request.getRequestURI();
+		String name = uri.substring("/reporters/".length());
+		
 		/* 테스트를 위한 수동 데이터 하드코딩 */
-		String name = "양현석";
 		String email = "fourwingsy@nhnnext.org";
 		String pressName = "nextian";
 		String info = "양현석 기자 / fourwingsy@nhnnext.org";
@@ -34,7 +43,15 @@ public class ShowReporter extends HttpServlet {
 		request.setAttribute("email", email);
 		request.setAttribute("pressName", pressName);
 		request.setAttribute("info", info);
-		request.getRequestDispatcher("ShowReporter.jsp").forward(request, response);
+		
+		/* ******** !!IMPORTANT!! **********
+		 * dispatch 시, 절대경로를 사용할 것!!!
+		 * 상대경로로 dispatch할 경우, /reporters/ShowReporter로 전달됨
+		 * 따라서 web.xml의 "/reporters/*"패턴에 붙잡혀 다시  서블릿으로 전달됨
+		 * 즉, 무한루프를 돌게 됨!!!!
+		 */
+		RequestDispatcher rd = request.getRequestDispatcher("/ShowReporter.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
