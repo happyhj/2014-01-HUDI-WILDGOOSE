d2d09967e2063488d2ba5692f9a34ebe91fd721a
Dec7 <cocagolau@gmail.com>
2014-04-17 01:02:10 +0900

    issue #169 프로젝트에 test 폴더구조 포함하여 젠킨스에서 개발서버 빌드 에러 수정

--- wildgoose/src/test/java/next/wildgoose/utility/UtilityTest.java
+++ wildgoose/src/test/java/next/wildgoose/utility/UtilityTest.java
@@ -1,14 +1,13 @@
 package next.wildgoose.utility;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class UtilityTest {
 
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
 	}
 
 }
