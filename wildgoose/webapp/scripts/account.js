2fed69e93ee9350c5c8404e4e7d33af898424968
Dec7 <cocagolau@gmail.com>
2014-05-05 12:59:32 +0900

    issue #271 front-end form validation 구현 완료

--- wildgoose/webapp/scripts/account.js
+++ wildgoose/webapp/scripts/account.js
@@ -43,6 +43,7 @@ function removeModal(event) {
 var accountBtn = document.querySelector(".account");
 accountBtn.addEventListener("click", doAction, false);
 
+
 /*
  * validation action
  */
@@ -54,30 +55,56 @@ function addValidationEvent() {
 		if (input.type == "email" || input.type == "password") {
 			
 			// blur event
-			input.addEventListener("blur", function(e) {
-					var me = e.target;
-					
-					// email
-					if (me.type == "email") {
-						if (!checkEmail(me)) {
-							Util.removeClass(me, "status-approved");
-							Util.addClass(me, "status-denied");
-						}
-						else {
-							// 서버에 존재하는지 확인
-							existInServer(me);
-						}
-					}
-					
-					// passward
-					
-					
-					// confirm
-					
-
-			}, false);
+			input.addEventListener("blur", checkSignUpFrom, false);
 		}
 	}
 }
 
-
+function checkSignUpFrom(e) {
+	var me = e.target;
+	var valid = false;
+	
+	// email
+	// e.valid가 undefined라는 의미는 사용자 정의 이벤트가 아니라는 의미임.
+	if (me.name == "email" && e.valid === undefined) {
+		// email이 유효할 경우 서버에 존재하는지 확인
+		if (validEmail(me)) {
+			existInServer(me);
+		}
+	}
+	// passward
+	if (me.name == "password") {
+		if (validPassword(me)) {
+			valid = true;
+		}
+	}
+	// confirm
+	if (me.name == "confirm") {
+		if (validConfirm(me)) {
+			valid = true;
+		}
+	}
+	
+	/*
+	 * 아이디의 사용가능여부를 확인하기 위해 ajax를 사용하여 서버에 검증하게 됨
+	 * ajax의 callback함수가 실행된 후 결과값을 받아오기 위해서
+	 * blur형식의 사용자 정의 이벤트를 발생만들고 valid라는 property속에 true/false라는 값을 포함하여
+	 * form input[name=email]에 전달함.
+	 * 
+	 * valid = e.valid는
+	 * 사용자 정의 이벤트일 경우에 
+	 * valid변수에 사용자 정의 이벤트 속에 담긴 valid property를 옮기는 작업을 의미함.
+	 */  
+	if (e.valid !== undefined) {
+		valid = e.valid;
+	}
+	
+	if (valid) {
+		Util.removeClass(me, "status-denied");
+		Util.addClass(me, "status-approved");
+	}
+	else {
+		Util.removeClass(me, "status-approved");
+		Util.addClass(me, "status-denied");
+	}
+}
