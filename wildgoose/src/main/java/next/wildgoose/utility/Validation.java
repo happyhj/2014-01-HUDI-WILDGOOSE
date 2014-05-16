9ffc4e3a290bb1eeb9f2ec82310d18a5058595ee
cpfl300 <cpfl300@gmail.com>
2014-05-16 16:37:39 +0900

    #255 회원가입 서버 #260 회원가입 클라이언트

--- wildgoose/src/main/java/next/wildgoose/utility/Validation.java
+++ wildgoose/src/main/java/next/wildgoose/utility/Validation.java
@@ -13,26 +13,6 @@ public class Validation {
 		return Pattern.matches(regex, URL);
 	}
 
-	public static boolean isEmail(String email) {
-		String regex = "^[\\w\\.-_\\+]+@[\\w-]+(\\.\\w{2,4})+$";
-
-		return Validation.isFilled(email) && Pattern.matches(regex, email);
-	}
-
-	public static boolean isPassword(String password) {
-		// client의 암호화 방식에 따라 전달되는 data의 형식을 추가 예정
-		
-		return Validation.isFilled(password);
-	}
 	
-	public static boolean isFilled(String data) {
-		
-		if (data.length() > 0) {
-			return true;
-		}
-		
-		return false;
-		
-	}
 
 }
