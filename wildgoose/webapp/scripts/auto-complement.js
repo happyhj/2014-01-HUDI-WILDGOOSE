6ffb7944f31a01320d2520ca42dde08fb35ac7b9
Dec7 <cocagolau@gmail.com>
2014-05-21 13:46:45 +0900

    issue #331 자동완성기능 리팩토링중 객체리터럴로 변환중.

--- wildgoose/webapp/scripts/auto-complement.js
+++ wildgoose/webapp/scripts/auto-complement.js
@@ -1,94 +1,212 @@
-// searched에 입력하고 있는 글자를 가져옴
-var searching = document.querySelector('#query-entry');
-var searched = document.querySelector(".searched-box tr");
-var currentRow = 0;
+var searchBox = {
+		init : function() {
+			this.listListener = false;
+			this.callbackRefCache = {
+					notify : this.notify.bind(this),
+					expired : this.expired.bind(this),
+					boxHandler : this.boxHandler.bind(this),
+					listHandler : this.listHandler.bind(this),
+					drawList : this.drawList.bind(this)
+			};
+			this.box = document.querySelector('#query-entry');
+			
+			// searched에 입력하고 있는 글자를 가져옴
+			this.list = document.querySelector(".searched-box");
+			this.currentRow = 0;
+			
+			this.box.addEventListener("focus", this.callbackRefCache.notify);
+			this.box.addEventListener("blur", this.callbackRefCache.expired);
+		},
+		
+		expired : function(evt) {
+			console.log("searchBox expired");
+			this.box.removeEventListener("keypress", this.callbackRefCache.boxHandler);
+			if (this.listListener) {
+				this.listListener = false;
+				this.box.removeEventListener("keyup", this.callbackRefCache.listHandler);
+			}
+		},
+		
+		notify : function(evt) {
+			console.log("searchBox standBy");
+			this.box.addEventListener("keypress", this.callbackRefCache.boxHandler);
+		},
+				
+		boxHandler : function(evt) {
+			console.log(this.listListener);
+			if (!this.listListener) {
+				this.listListener = true;
+				console.log(this.list);
+				this.box.addEventListener("keyup", this.callbackRefCache.listHandler);
+			}
+		    this.find(this.box.value);
+		},
+		
+		listHandler : function(evt) {
+			var keyID = evt.keyCode;
+			console.log(keyID);
+			// UP/DOWN Key
+		    if (keyID == 38 || keyID == 40) {
+//		        this.highlightRow(39 - keyID);
+		    	console.log("press up down key");
+		        return;
+		    }
+		    if (keyID == 13) {
+//		    	this.selectRow();
+		    	console.log("select row");
+		    }
+		},
 
-searching.onkeyup = function(e) {
+		on : function() {
+			this.box.style.display = "table";
+		},
+		
+		off : function() {
+			this.box.style.display = "none";
+		},
+		
+		
+		find : function (searched) {
+			var url = "/api/v1/search/most_similar_names?name=" + searched;
+			Ajax.GET(url, this.callbackRefCache.drawList);
+		},
+		
+		drawList : function(data) {
+			var Data = JSON.parse(data)["data"];
+			var box = document.querySelector(".searched-box");
+			box.style.display = "table";
+			if(typeof Data != "undefined" && Data.length != 0) {
+				var tr_pos = document.querySelector(".searched-box").lastElementChild.lastElementChild;
+				var childCount = tr_pos.childElementCount;
+				if(childCount != 0){
+					for(var i = 0 ; i < childCount; i++){
+						tr_pos.removeChild(tr_pos.childNodes[0]);
+					}
+				}
+				
+				for(var i = 0 ; i < Data.length; i++){
+					tr_pos.insertAdjacentHTML('beforeend', '<td>'+Data[i]["name"]+'</td>');
+				}
+			}
+		},
+		selectRow : function() {
+//		    var searched = document.querySelector(".searched-box");
+		    var possibilities = searched.querySelectorAll("td");
+		    
+//		    evt = document.createEvent("MouseEvents");
+//		    evt.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+//		    possibilities[currentRow].dispatchEvent(evt)
+		},
+		
+		highlightRow : function(change) {
+		    var searched = document.querySelector(".searched-box");
+		    var possibilities = searched.querySelectorAll("td");
+		    
+		    evt = document.createEvent("MouseEvents");
+		    evt.initMouseEvent("mouseout", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		    possibilities[currentRow].dispatchEvent(evt)
+		    
+		    currentRow -= change;
+		    if (currentRow >= 5) {
+		        currentRow -= 5;
+		    } else if (currentRow < 0) {
+		        currentRow += 5;
+		    }
+		    evt = document.createEvent("MouseEvents");
+		    evt.initMouseEvent("mouseover", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		    possibilities[currentRow].dispatchEvent(evt)
+		}
+		//		toggle : function(evt) {
+		////	debugger;
+		//	if(getComputedStyle(searchBox.box, null).display == "none"){
+		//		this.on();
+		//	} else {
+		//		this.off();
+		//	}
+		//},
+
+}
+//debugger;
+// searchBox에 focus evt가 발생시 searchBox객체 init
+searchBox.init();
+
+
+//searching.addEventListener("keyup", searchBoxHandler);
+//searching.addEventListener("click", toggleSearchBox);
+
+
+
+function searchBoxHandler(e) {
 	var keyID = e.keyCode; 
     // UP/DOWN Key
     if (keyID == 38 || keyID == 40) {
         highlightRow(39 - keyID);
         return;
-    } else if (keyID == 13) {
+    }
+    if (keyID == 13) {
         selectRow();
     }
-	var url = "/api/v1/search/most_similar_names?name=";
-	var searched = searching.value;
-	url = url + searched;
+    
+    searchSimilarKeyword(searching.value);
+}
+
+function searchSimilarKeyword(searched) {
+	var url = "/api/v1/search/most_similar_names?name=" + searched;
 	Ajax.GET(url, drawBox);
 }
 
-searched.addEventListener("mouseover", function(e) {
-	var target = e.target;
-	target.className = "mouseover";
-})
-searched.addEventListener("mouseout", function(e) {
-	var target = e.target;
-	target.className = "";
-})
-
-function selectRow() {
-    var searched = document.querySelector(".searched-box");
-    var possibilities = searched.querySelectorAll("td");
-    
-    evt = document.createEvent("MouseEvents");
-    evt.initMouseEvent("click", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    possibilities[currentRow].dispatchEvent(evt)
-}
-function highlightRow(change) {
-    var searched = document.querySelector(".searched-box");
-    var possibilities = searched.querySelectorAll("td");
-    
-    evt = document.createEvent("MouseEvents");
-    evt.initMouseEvent("mouseout", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    possibilities[currentRow].dispatchEvent(evt)
-    
-    currentRow -= change;
-    if (currentRow >= 5) {
-        currentRow -= 5;
-    } else if (currentRow < 0) {
-        currentRow += 5;
-    }
-    evt = document.createEvent("MouseEvents");
-    evt.initMouseEvent("mouseover", true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    possibilities[currentRow].dispatchEvent(evt)
-}
-
-// 추천 검색어를 박스에 써 넣음
-function drawBox(data) {
-	var Data = JSON.parse(data)["data"];
-	var box = document.querySelector(".searched-box");
-	box.style.display = "table";
-	if(typeof Data != "undefined" && Data.length != 0) {
-		var tr_pos = document.querySelector(".searched-box").lastElementChild.lastElementChild;
-		var childCount = tr_pos.childElementCount;
-		if(childCount != 0){
-			for(var i = 0 ; i < childCount; i++){
-				tr_pos.removeChild(tr_pos.childNodes[0]);
-			}
-		}
-		
-		for(var i = 0 ; i < Data.length; i++){
-			tr_pos.insertAdjacentHTML('beforeend', '<td>'+Data[i]["name"]+'</td>');
-		}
-	}
-}
-
 //검색창이 클릭하면 추천 검색어 박스가 보였다 보이지 않았다 함
-searching.onclick = function() {
-	var box = document.querySelector(".searched-box");
-	if(getComputedStyle(box, null).display == "none"){
-		box.style.display = "table";
-	} else {
-		box.style.display = "none";
-	}
-}
+//function toggleSearchBox() {
+//	var box = document.querySelector(".searched-box");
+//	if(getComputedStyle(box, null).display == "none"){
+//		box.style.display = "table";
+//	} else {
+//		box.style.display = "none";
+//	}
+//}
 
-// 클릭하면 검색어 입력
-document.querySelector(".searched-box").addEventListener('click', insetText, false);
-function insetText(e) {
-	if(e.target.tagName == "TD") {
-		searching.value = e.target.textContent
-		document.querySelector(".searched-box").style.display = "none";
-	}
-}
+
+
+
+
+
+//var searched = document.querySelector(".searched-box tr");
+//searched.addEventListener("mouseover", function(e) {
+//	var target = e.target;
+//	target.className = "mouseover";
+//});
+//searched.addEventListener("mouseout", function(e) {
+//	var target = e.target;
+//	target.className = "";
+//});
+//
+
+//
+//// 추천 검색어를 박스에 써 넣음
+//function drawBox(data) {
+//	var Data = JSON.parse(data)["data"];
+//	var box = document.querySelector(".searched-box");
+//	box.style.display = "table";
+//	if(typeof Data != "undefined" && Data.length != 0) {
+//		var tr_pos = document.querySelector(".searched-box").lastElementChild.lastElementChild;
+//		var childCount = tr_pos.childElementCount;
+//		if(childCount != 0){
+//			for(var i = 0 ; i < childCount; i++){
+//				tr_pos.removeChild(tr_pos.childNodes[0]);
+//			}
+//		}
+//		
+//		for(var i = 0 ; i < Data.length; i++){
+//			tr_pos.insertAdjacentHTML('beforeend', '<td>'+Data[i]["name"]+'</td>');
+//		}
+//	}
+//}
+//
+//// 클릭하면 검색어 입력
+//document.querySelector(".searched-box").addEventListener('click', insetText, false);
+//function insetText(e) {
+//	if(e.target.tagName == "TD") {
+//		searching.value = e.target.textContent
+//		document.querySelector(".searched-box").style.display = "none";
+//	}
+//}
