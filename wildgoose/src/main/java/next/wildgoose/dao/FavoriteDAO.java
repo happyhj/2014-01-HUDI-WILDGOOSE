7e4c6d9f8b7a29e198f030ce612bdc641aebdc84
Dec7 <cocagolau@gmail.com>
2014-05-27 00:31:51 +0900

    #392 favoriteDAO를 jdbcTemplate을 사용하여 리팩토링

--- wildgoose/src/main/java/next/wildgoose/dao/FavoriteDAO.java
+++ wildgoose/src/main/java/next/wildgoose/dao/FavoriteDAO.java
@@ -1,13 +1,11 @@
 package next.wildgoose.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import next.wildgoose.database.DataSource;
 import next.wildgoose.dto.ReporterCard;
 
 import org.slf4j.Logger;
@@ -16,149 +14,106 @@ import org.slf4j.LoggerFactory;
 public class FavoriteDAO {
 	private static final Logger LOGGER = LoggerFactory.getLogger(FavoriteDAO.class.getName());
 
-	public boolean addFavorite(String reporterId, String email) {
-		boolean result = false;
-		Connection conn = DataSource.getInstance().getConnection();
-		PreparedStatement psmt = null;
-
+	public boolean addFavorite(final String reporterId, final String email) {
+		
+		InsertJdbcTemplate template = new InsertJdbcTemplate () {
+			
+			@Override
+			public void setValues(PreparedStatement psmt) throws SQLException {
+				psmt.setString(1, email);
+				psmt.setString(2, reporterId);
+			}
+		};
+		
 		StringBuilder query = new StringBuilder();
 		query.append("INSERT INTO favorite (user_email, author_id) VALUES(?,?);");
-
-		try {
-			psmt = conn.prepareStatement(query.toString());
-			psmt.setString(1, email);
-			psmt.setString(2, reporterId);
-			psmt.execute();
-			result = true;
-		} catch (SQLException sqle) {
-			LOGGER.debug(sqle.getMessage(), sqle);
-		} finally {
-			SqlUtil.closePrepStatement(psmt);
-			SqlUtil.closeConnection(conn);
-		}
-		return result;
-	}
-
-	public boolean removeFavorite(String reporterId, String email) {
-		boolean result = false;
-		Connection conn = DataSource.getInstance().getConnection();
-		PreparedStatement psmt = null;
-
-		StringBuilder query = new StringBuilder();
-		query.append("DELETE FROM favorite WHERE user_email =? AND author_id =?;");
-
-		try {
-			psmt = conn.prepareStatement(query.toString());
-			psmt.setString(1, email);
-			psmt.setString(2, reporterId);
-			psmt.execute();
-			result = true;
-		} catch (SQLException sqle) {
-			LOGGER.debug(sqle.getMessage(), sqle);
-		} finally {
-			SqlUtil.closePrepStatement(psmt);
-			SqlUtil.closeConnection(conn);
-		}
-		return result;
+		
+		return template.insert(query.toString());
 	}
 	
-	public List<Integer> getFavorites(String email) {
-		List<Integer> favorites = new ArrayList<Integer>();
-		Connection conn = DataSource.getInstance().getConnection();
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
+	
+	public boolean removeFavorite(final String reporterId, final String email) {
+		
+		DeleteJdbcTemplate template = new DeleteJdbcTemplate() {
 
+			@Override
+			public void setValues(PreparedStatement psmt) throws SQLException {
+				psmt.setString(1, email);
+				psmt.setString(2, reporterId);
+			}
+		};
+		
+		StringBuilder query = new StringBuilder();
+		query.append("DELETE FROM favorite WHERE user_email =? AND author_id =?;");
+		
+		return template.delete(query.toString());
+	}
+	
+	
+	public List<Integer> getFavorites(final String email) {
+		
+		SelectJdbcTemplate template = new SelectJdbcTemplate() {
+
+			@Override
+			Object mapRow(ResultSet rs) throws SQLException {
+				List<Integer> favorites = new ArrayList<Integer>();
+				
+				while (rs.next()) {
+					favorites.add(rs.getInt("author_id"));
+				}
+				return favorites;
+			}
+
+			@Override
+			void setValues(PreparedStatement psmt) throws SQLException {
+				psmt.setString(1, email);
+			}
+		};
 		StringBuilder query = new StringBuilder();
 		query.append("SELECT author_id FROM favorite WHERE user_email = ?");
+		List<Integer> favorites = (List<Integer>) template.select(query.toString());
 
-		try {
-			psmt = conn.prepareStatement(query.toString());
-			psmt.setString(1, email);
-			rs = psmt.executeQuery();
-
-			while (rs.next()) {
-				favorites.add(rs.getInt("author_id"));
-			}
-		} catch (SQLException sqle) {
-			LOGGER.debug(sqle.getMessage(), sqle);
-		} finally {
-			SqlUtil.closeResultSet(rs);
-			SqlUtil.closePrepStatement(psmt);
-			SqlUtil.closeConnection(conn);
-		}
 		return favorites;
 	}
 	
-	public List<ReporterCard> findReporterCard(String email) {		
-		Connection conn = DataSource.getInstance().getConnection();
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
-		List<ReporterCard> favorites = new ArrayList<ReporterCard>();
-		ReporterCard reporterCard = null;
-		
+	public List<ReporterCard> findReporterCard(final String email) {
+		SelectJdbcTemplate template = new SelectJdbcTemplate() {
 
+			@Override
+			Object mapRow(ResultSet rs) throws SQLException {
+				List<ReporterCard> favorites = new ArrayList<ReporterCard>();
+				ReporterCard reporter = null;
+				
+				while (rs.next()) {
+					reporter = new ReporterCard();
+					reporter.setId(rs.getInt("id"));
+					reporter.setEmail(rs.getString("email"));
+					reporter.setName(rs.getString("name"));
+					reporter.setPressName(rs.getString("press_name"));
+					reporter.setArticleTitle(rs.getString("article_title"));
+					reporter.setArticleURL(rs.getString("article_url"));
+					
+					favorites.add(reporter);
+				}
+				return favorites;
+			}
+
+			@Override
+			void setValues(PreparedStatement psmt) throws SQLException {
+				psmt.setString(1, email);
+				//psmt.setInt(2, start);
+				//psmt.setInt(3, num);
+			}
+		};
+		
 		StringBuilder query = new StringBuilder();
 		query.append("SELECT author.* FROM (SELECT * FROM favorite WHERE user_email = ? ) AS myfav JOIN ");
 		query.append("(SELECT result.id as id, result.name as name, result.email as email, article.title as article_title, press.name as press_name, result.article_URL ");
 		query.append("FROM (SELECT * FROM author JOIN article_author AS aa ON author.id = aa.author_id GROUP BY author.id ORDER BY author.name) as result ");
 		query.append("JOIN article ON article.URL = result.article_URL JOIN press ON result.press_id = press.id) AS author ON author.id = myfav.author_id LIMIT 24");
-
 		
-		try {
-			psmt = conn.prepareStatement(query.toString());
-			psmt.setString(1, email);
-			//psmt.setInt(2, start);
-			//psmt.setInt(3, num);
-			rs = psmt.executeQuery();
-			
-			while (rs.next()) {
-				reporterCard = new ReporterCard();
-				reporterCard.setId(rs.getInt("id"));
-				reporterCard.setEmail(rs.getString("email"));
-				reporterCard.setName(rs.getString("name"));
-				reporterCard.setPressName(rs.getString("press_name"));
-				reporterCard.setArticleTitle(rs.getString("article_title"));
-				reporterCard.setArticleURL(rs.getString("article_url"));
-
-				favorites.add(reporterCard);
-			}
-		} catch (SQLException sqle) {
-			LOGGER.debug(sqle.getMessage(),sqle);
-			favorites = null;
-		} finally {
-			SqlUtil.closePrepStatement(psmt);
-			SqlUtil.closeResultSet(rs);
-			SqlUtil.closeConnection(conn);
-		}
+		List<ReporterCard> favorites = (List<ReporterCard>) template.select(query.toString());
+		
 		return favorites;
 	}
-	
-	/*
-	public JSONObject getFavoritesAsJson(String email) {
-		JSONObject result = new JSONObject();
-		Connection conn = DataSource.getInstance().getConnection();
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
-		
-		StringBuilder query = new StringBuilder();
-		query.append("SELECT author_id FROM favorite WHERE user_email = ?");
-
-		try {
-			psmt = conn.prepareStatement(query.toString());
-			psmt.setString(1, email);
-			rs = psmt.executeQuery();
-			
-			while (rs.next()) {
-				result.append("data", rs.getInt("author_id"));
-			}
-		} catch (SQLException sqle) {
-			LOGGER.debug(sqle.getMessage(), sqle);
-		} finally {
-			SqlUtil.closeResultSet(rs);
-			SqlUtil.closePrepStatement(psmt);
-			SqlUtil.closeConnection(conn);
-		}
-		return result;
-	} */
-
 }
